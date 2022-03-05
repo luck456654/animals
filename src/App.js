c49af252f291f3c9d8cdbe0005782d7e892ca4e3
Vadim <luckjantsev@rambler.ru>
2022-03-05 23:31:53 +0300

    2

--- src/App.js
+++ src/App.js
@@ -1,53 +1,18 @@
-import axios from 'axios'
-import './App.css';
-import React, { useState } from 'react';
-import {
-  BrowserRouter,
-  Switch,
-  Route,
-  Link,
-} from 'react-router-dom';
-import Authorization from './components/Authorization.jsx';
-import Home from './components/Home.jsx';  
+import axios from "axios";
+import "./App.css";
+import React, { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Authorization from "./components/Authorization.jsx";
+import Home from "./components/Home.jsx";
 
 function App() {
-  let[token,setToken]=useState()
-  let[login,setLogin]=useState()
-  let[password,setPassword]=useState()
-  let[formAuth,setFormAuth]=useState(
-  
-  )
-  function auth(){
-    const url = 'https://acits-test-back.herokuapp.com/api/login/'
-    axios.post(url, {login:login,password:password}, {
-      dataType: 'json',
-      'Access-Control-Allow-Origin': url,
-      'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE'
-    })
-    .then( res => setToken(res.data.accessToken))
-  }
-  function handleChangeLogin(event) {
-		setLogin(event.target.value);
-	}
-  function handleChangePassword(event) {
-		setPassword(event.target.value);
-	}
-  
   return (
     <BrowserRouter>
       <div className="App">
-         <Route exact path='/auth' component={Authorization}>
-            <Authorization 
-            login={login} 
-            handleChangeLogin={handleChangeLogin} 
-            password={password}
-            handleChangePassword={handleChangePassword}
-            auth={auth}
-            />
-          </Route>
-          <Route exact path='/home' component={Home}>
-            <Home />
-          </Route>
+        <Routes>
+          <Route exact path="/auth" element={<Authorization />}></Route>
+          <Route exact path="/home" element={<Home />}></Route>
+        </Routes>
       </div>
     </BrowserRouter>
   );
--- src/components/Authorization.jsx
+++ src/components/Authorization.jsx
@@ -1,8 +1,64 @@
-function Authorization(props) {
-    return <div>
-  <input type='text' value={props.login} onChange={props.handleChangeLogin} placeholder='Введите логин'></input>
-  <input type='text' value={props.password} onChange={props.handleChangePassword} placeholder='Введите пароль'></input>
-  <input type="button" value="Авторизоваться" onClick={props.auth}></input>
-</div>
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { createBrowserHistory } from 'history';
+import { useNavigate } from 'react-router-dom';
+
+function Authorization() {
+  const [login, setLogin] = useState();
+  const [password, setPassword] = useState();
+  const [formAuth, setFormAuth] = useState();
+  const [token, setToken] = useState(null);
+
+  const history = createBrowserHistory();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (token) {
+      history.push("/home");
+      navigate('/home')
+      }
+    else{
+        alert("Вы не авторизованны");
+    }
+  }, [token]);
+
+  function auth() {
+    const url = "https://acits-test-back.herokuapp.com/api/login/";
+    axios
+      .post(
+        url,
+        { login: login, password: password },
+        {
+          dataType: "json",
+          "Access-Control-Allow-Origin": url,
+          "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE",
+        }
+      )
+      .then((res) => setToken(res.data.accessToken));
   }
-  export default Authorization;
+
+  function handleChangeLogin(event) {
+    setLogin(event.target.value);
+  }
+  function handleChangePassword(event) {
+    setPassword(event.target.value);
+  }
+  return (
+    <div>
+      <input
+        type="text"
+        value={login}
+        onChange={handleChangeLogin}
+        placeholder="Введите логин"
+      ></input>
+      <input
+        type="text"
+        value={password}
+        onChange={handleChangePassword}
+        placeholder="Введите пароль"
+      ></input>
+      <input type="button" value="Авторизоваться" onClick={auth}></input>
+      {token}
+    </div>
+  );
+}
+export default Authorization;
